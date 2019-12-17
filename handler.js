6f7c7905c0f1112e03da5006828f930135027a32
armandor10 <armandorp10@gmail.com>
2019-12-17 00:59:48 -0500

    Dynamodb

--- handler.js
+++ handler.js
@@ -1,19 +1,28 @@
 'use strict';
+const databaseManager = require('./databaseManager');
 
-module.exports.hello = async event => {
-  console.log(event);
-  let message = event.queryStringParameters && event.queryStringParameters.name?
-                      'Hello ' + event.queryStringParameters.name:
-                      "Hello world!";
+function createResponse(statusCode, message) {
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: message,
-        input: event,
-      },
-      null,
-      2
-    ),
+    statusCode: statusCode,
+    body: JSON.stringify(message)
   };
+}
+
+module.exports.hello = (event, context, callback) => {
+  const itemId = event.queryStringParameters.name;
+  databaseManager.getItem(itemId).then(response => {
+    console.log(response);
+    callback(null, createResponse(200, response));
+  });
+};
+
+module.exports.save = (event, context, callback) => {
+  const item = JSON.parse(event.body);
+  console.log(item);
+  item.itemId = item.name;
+
+  databaseManager.saveItem(item).then(response => {
+    console.log(response);
+    callback(null, createResponse(200, response));
+  });
 };
